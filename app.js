d2015c4f9c2896ff66b86162a06b2fd4fef43355
Jonatas Saraiva <jonatas.saraiva@gmail.com>
2016-01-07 11:53:03 -0200

    Add get /

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ var app = express();
 
 app.use(express.static('./'));
 
+app.get('/', function(request, response) {
+  response.send('OK request');
+});
+
 var port = process.env.PORT || 3000;
 app.listen(port, function () {
 	console.log('Running API on localhost:' + port);
